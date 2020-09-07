23b308c87ca6333eadd0959a58a4ee32767b1a4c
Maick Souza <maick_a_s@msn.com>
2020-09-07 18:58:28 -0300

    chore(web): Lack of stock checking

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -3,7 +3,8 @@ import { IProduct } from '../store/modules/cart/types';
 
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
 
 interface CatalogItemProps {
   product: IProduct;
@@ -11,6 +12,10 @@ interface CatalogItemProps {
 
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  })
 
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product))
@@ -27,6 +32,8 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
         >
           Comprar
         </button>
+
+        { hasFailedStockCheck && <span style={{ color: 'red' }}>Falta de estoque</span> }
       </article>
   );
 }
